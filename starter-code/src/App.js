b72f5b0d0bae848761888fa23e99c75063568ed7
Otto Schmid <otto.schmid@Ottos-MacBook-Pro.local>
2020-04-16 22:29:44 +0200

    Added Search ;)

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -15,13 +15,19 @@ class App extends Component {
     super(props)
     this.state = {
       beers: [],
+      searchString: '',
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
     axios.get('https://ih-beers-api2.herokuapp.com/beers').then((res) => {
       this.setState({ beers: res.data })
     })
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchString: e.target.value })
   }
 
   render() {
@@ -38,9 +44,20 @@ class App extends Component {
           <div>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/beers" render={(props) => <Beers {...props} beers={this.state.beers} />} />
-              <Route exact path="/beers/:id" render={(props) => <Beer {...props} beers={this.state.beers}/>} />
-              <Route path="/random-beer" render={(props) => <RandomBeer {...props} beers={this.state.beers}/>} />
+              <Route
+                exact
+                path="/beers"
+                render={(props) => (
+                  <Beers
+                    {...props}
+                    beers={this.state.beers}
+                    handleChange={this.handleChange}
+                    search={this.state.searchString}
+                  />
+                )}
+              />
+              <Route exact path="/beers/:id" render={(props) => <Beer {...props} beers={this.state.beers} />} />
+              <Route path="/random-beer" render={(props) => <RandomBeer {...props} beers={this.state.beers} />} />
               <Route path="/new-beer" component={NewBeer} />
             </Switch>
           </div>
--- starter-code/src/components/Beers.js
+++ starter-code/src/components/Beers.js
@@ -2,29 +2,44 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 import { Link } from 'react-router-dom'
 
 export default function Beers(props) {
   return (
     <div>
       <Container>
-        {props.beers.map((beer, idx) => (
-          <Card key={idx} style={{ margin: '5px' }}>
-            <Card.Img variant="top" src={beer.image_url} style={{ width: '20%', margin: '20px' }} />
-            <Card.Body>
-              <Card.Title>{beer.name}</Card.Title>
-              <Card.Text>{beer.tagline}</Card.Text>
-              <Link to={`/beers/${beer._id}`}>
-                <Button variant="secondary" size="sm" block>
-                  See Details
-                </Button>
-              </Link>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Crafted by {beer.contributed_by}</small>
-            </Card.Footer>
-          </Card>
-        ))}
+        <InputGroup className="m-3" style={{ left: '-16px' }}>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Search Beer Name</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            type="text"
+            name="search"
+            value={props.search}
+            onChange={(e) => props.handleChange(e)}
+          />
+        </InputGroup>
+        {props.beers
+          .filter((beer) => beer.name.toUpperCase().includes(props.search.toUpperCase()))
+          .map((beer, idx) => (
+            <Card key={idx} style={{ margin: '5px' }}>
+              <Card.Img variant="top" src={beer.image_url} style={{ width: '20%', margin: '20px' }} />
+              <Card.Body>
+                <Card.Title>{beer.name}</Card.Title>
+                <Card.Text>{beer.tagline}</Card.Text>
+                <Link to={`/beers/${beer._id}`}>
+                  <Button variant="secondary" size="sm" block>
+                    See Details
+                  </Button>
+                </Link>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Crafted by {beer.contributed_by}</small>
+              </Card.Footer>
+            </Card>
+          ))}
       </Container>
     </div>
   )
